f6d77f1bec2d141d8c8cf5f508d1a477e507d31a
Benjamin Schulte <bs@golfgl.de>
2022-06-03 15:13:32 +0200

    Init project from Spring initializr

---
+++ src/main/kotlin/org/ergoplatform/mosaik/example/ageusd/AgeusdApplication.kt
@@ -0,0 +1,11 @@
+package org.ergoplatform.mosaik.example.ageusd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgeusdApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgeusdApplication>(*args)
+}
---
+++ src/test/kotlin/org/ergoplatform/mosaik/example/ageusd/AgeusdApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ergoplatform.mosaik.example.ageusd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgeusdApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
